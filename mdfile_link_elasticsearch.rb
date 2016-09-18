20dabceaa937494ee0156aa0665b02f5422fc3c6
Hirotaka Suzuki <suhirotaka@gmail.com>
2016-09-18 20:27:30 +0900

    mdfile_link_elasticsearch.rb: Implement basic functions

--- mdfile_link_elasticsearch.rb
+++ mdfile_link_elasticsearch.rb
@@ -1,16 +1,58 @@
 require 'elasticsearch'
 require 'open-uri'
+require 'securerandom'
 
-esc = Elasticsearch::Client.new
+class MdfileLinkElasticsearch
+  def initialize
+    @esc = Elasticsearch::Client.new
+    
+    # Set Elasticsearch index name
+    @index_name = ''
+    begin
+      @index_name = "ruby-readme-#{SecureRandom.hex(8)}"
+    end while @esc.indices.exists? index: @index_name
 
-data = File.read('./readme_test.dat')
-matched = data.scan(/\[.+?\].*?\((.+?)\)/)
-matched.each do |url|
-  body = open(url.first, &:read)
-  esc.create index: 'readme', type: 'article', body:{url: url.first, html_content: body}
-  break
+    # Set destructor
+    #ObjectSpace.define_finalizer(self, self.cleanup_index)
+  end
+  
+  def get_articles
+    # Get articles from md file
+    filename = './readme_test.dat'
+    data = File.read(filename)
+    matches = data.scan(/\[(.+?)\].*?\((.+?)\)/)
+    matches.each do |match|
+      title = match.first
+      url = match.last
+      begin
+        body = open(url, &:read).encode('UTF-8')
+      rescue Encoding::UndefinedConversionError => e
+        p e
+      end
+      @esc.create index: @index_name, type: 'article', body: { title: title, url: url, html_content: body }
+#break
+    end
+  end
+  
+  def send_query
+    # Query to Elasticsearch
+    query = ARGV[0]
+    #res = @esc.search index: @index_name, type: 'article', q: query
+    res = @esc.search index: @index_name, type: 'article', body: { query: { match: { html_content: query } } }
+    res['hits']['hits'].each do |article|
+      p "#{article['_source']['title']}(#{article['_source']['url']})"
+    end
+    cleanup_index
+  end
+  
+  def cleanup_index
+    # Cleanup the index
+    p "Cleanup Elasticsearch index: #{@index_name}"
+    @esc.indices.delete index: @index_name
+  end
 end
-p esc.search index: 'readme', type: 'article', q: 'html'
-exit
 
-p c.info
+md_elastic = MdfileLinkElasticsearch.new
+md_elastic.get_articles
+sleep(10)
+md_elastic.send_query
