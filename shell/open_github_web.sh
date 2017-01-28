5ddc5fa5b395aacd28a2513e7a6756d84e6a20ec
Hirotaka Suzuki <suhirotaka@gmail.com>
2017-01-28 23:16:30 +0900

    improved for general use

--- shell/open_github_web.sh
+++ shell/open_github_web.sh
@@ -5,9 +5,11 @@ VERSION_CODE=1.0.0
 # show usage
 usage() {
   cat <<__EOS__
-$(basename $0) opens github web page from local repository
+$(basename $0) opens github web page from local repository.
 
-Usage: $(basename $0) <REMOTE_NAME>
+Usage: $(basename $0) [<REMOTE_NAME>]
+
+If no REMOTE_NAME is given, "origin" will be used.
 
 Options:
   --help     Print this
@@ -15,29 +17,42 @@ Options:
 __EOS__
 }
 
+if [ -n "$2" ]; then
+  echo 'Error: invalid options'
+  exit 1
+fi
+
 case "$1" in
-  "-h" | "--help" )
+  -h | --help )
     usage
     exit 0
     ;;
 
-  "--version" )
+  -v | --version )
     echo "$(basename $0) version $VERSION_CODE"
     exit 0
+    ;;
+
+  -* )
+    echo 'Error: invalid options'
+    exit 1
     ;;
 esac
 
 remote_name=$1
 if [ -z "$remote_name" ]; then
-  usage
-  exit 1
+  remote_name=origin
 fi
 
 cd .
-github_url=$(git remote -v show $remote_name | grep "Fetch URL:" | grep -o -E "http[s]*:\/\/.+$")
+git_remote="$(git remote -v show $remote_name)"
+github_url=$(echo "$git_remote" | grep -E "Fetch[ ]+URL:" | grep -o -E "http[s]*:\/\/.+$")
+if [ -z "$github_url" ]; then
+  github_url=$(echo "$git_remote" | grep -E "Push[ ]+URL:" | grep -o -E "http[s]*:\/\/.+$")
+fi
 
 if [ -z "$github_url" ]; then
-  echo 'Invalid url'
+  echo 'Error: invalid url'
 else
   echo "opening $github_url ..."
   open $github_url
