33e4e0c51685d87a9c394add03f7d0e3ee95afe3
Hirotaka Suzuki <suhirotaka@gmail.com>
2017-10-13 17:19:35 +0900

    bugfix shell/excel_to_csv.sh

--- shell/excel_to_csv.sh
+++ shell/excel_to_csv.sh
@@ -2,10 +2,10 @@
 
 in_dir=$1
 out_dir=$2
-in_file_name='*somefile*'
+in_file_name='*'
 
-find "$in_dir" -type d -name $in_file_name -print0 | while IFS= read -r -d '' excel_dir; do
-echo $excel_dir
+find "$in_dir" -type d -name "$in_file_name" -print0 | while IFS= read -r -d '' excel_dir; do
+  echo $excel_dir
   find "$excel_dir" -type f -name '*xls*' -print0 | while IFS= read -r -d '' in_file; do
     out_file_name="$(echo $(basename "$in_file") | sed 's/\.[^\.]*$//').csv"
     xlsx2csv "$in_file" "$out_dir/$out_file_name"
